243adcf8cebd117d2c494d8fd52ab585fb5291d4
adityanegara <aditnegarabisnis@gmail.com>
2021-07-13 13:48:13 +0700

    Immplementing Cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
     KEY : 'b93cf6337ffed1c21b9150e0397addf2',
     BASE_URL : 'https://api.themoviedb.org/3/',
     BASE_IMAGE_URL : 'https://image.tmdb.org/t/p/w500/',
-    DEFAULT_LANGUAGE : 'en-us'
+    DEFAULT_LANGUAGE : 'en-us',
+    CACHE_NAME : new Date().toISOString(),
 };
 
 export default CONFIG;
