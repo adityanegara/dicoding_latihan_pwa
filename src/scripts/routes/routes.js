58f0b3d52e45ae08b18ade2ad35d552ff3839ff2
adityanegara <aditnegarabisnis@gmail.com>
2021-07-14 15:27:17 +0700

    Implementing : Indexed DB Favorite Movie

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import NowPlaying from '../views/pages/now-playing';
 import Upcoming from '../views/pages/upcoming';
 import Detail from '../views/pages/detail';
 import Home from '../views/pages/home';
- 
+import Like from '../views/pages/like'; 
+
 const routes = {
   '/': Home, // default page
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
   '/detail/:id': Detail,
+  '/like': Like,
 };
  
 export default routes;
